903a0bb5b34083f512249ad255739bc650471953
kazuki70102 <kazuki70102@gmail.com>
2019-11-02 16:59:12 +0900

    add PostTest

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+
+
+    public function testCreatePost(): void
+    {
+        $response = $this->actingAs($this->user);
+        $response = $this->json('POST', route('post.store'), ['content' => 'こんにちは！！']);
+
+        $response->assertRedirect(route('profile.index'));
+        $this->assertDatabaseHas('posts', [
+            'user_id' => $this->user->id,
+            'content' => 'こんにちは！！'
+        ]);
+    }
+}
